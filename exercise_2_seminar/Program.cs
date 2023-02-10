f3928af43dc02232aadfd38c3dfdb8da704d179e
Anna Lutsevich <alucevich@mail.ru>
2023-02-10 22:14:42 +0700

    Add folder   exercise_2_seminar

---
+++ exercise_2_seminar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа на вход принимает два числа и проверяет, является ли первое квадратом второго
+
+Console.Write("Введите первое число  ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число  ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 *number2 == number1)
+{
+    Console.WriteLine("Квадрат второго числа равен первому числу   - TRUE");
+}
+else
+{
+    Console.WriteLine("Квадрат второго числа НЕ равен первому числу   -  FALSE");
+}
