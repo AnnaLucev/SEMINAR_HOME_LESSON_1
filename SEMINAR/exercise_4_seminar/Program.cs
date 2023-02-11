6d1ea4822202a42b7640fcd5a96fa12c770e2e61
Anna Lutsevich <alucevich@mail.ru>
2023-02-11 15:29:32 +0700

    Add home_lesson_1

---
+++ SEMINAR/exercise_4_seminar/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Программа на вход принимает N, на выходе показывает все целые числа в промежутки от -N до N
+
+Console.Write("Введите число  ");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine())); // Math.Abs() - дает модуль числа
+
+// Первый способ
+//
+// for (int i = -a; i <= a; i++)
+// {
+//    Console.WriteLine(i);
+// }
+// Второй способ
+//
+int i = -a;
+while (i <= a) {
+    Console.WriteLine(i);
+    i++;
+}
---
+++ SEMINAR/exercise_5_seminar/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.Write("Введите число  ");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine())); // Math.Abs() - дает модуль числа
+
+if (a / 100 == 0 || a / 100 > 9) {
+    Console.WriteLine("Ввод числа не корректен / This number is not correct");
+}
+else {
+    int result = a % 10;
+    Console.WriteLine($"Последняя цифра числа  {a} является  {result}");
+}
